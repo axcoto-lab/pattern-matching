2a86d10932cdcb6939a9b4ff9d47814891d473b1
Vinh <kurei@axcoto.com>
2016-02-21 20:49:44 -0800

    Add ignore filters
    Value that matches ignore filters are ignore

--- lib/pm/finder.rb
+++ lib/pm/finder.rb
@@ -22,23 +22,30 @@ module PM
       analyzer.analyze(@hostnames)
     end
 
-    def self.work!(source)
+    def self.work!(args)
+      source = args[:file]
       pm = PM::Finder.new(PM::Input.read(source))
 
       host_segments = pm.analyze
       sorted_words = pm.count(host_segments)
 
-      sorted_words.reject! { |v| v[1] <= THRESHOLD }
+      #sorted_words.reject! { |v| v[1] <= THRESHOLD }
 
       pp sorted_words
       pm
     end
 
+    def ignore_filters?(value)
+      value.match(/^[0-9]+$/)
+    end
+
     def count(array)
       occurences = {}
       array.each do |value|
+        next if ignore_filters?(value)
+
         value.downcase!
-        if occurences[value.downcase].nil?
+        if occurences[value].nil?
           occurences[value] = 1
         else
           occurences[value] += 1
